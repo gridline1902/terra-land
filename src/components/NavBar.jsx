21e285b5844db387cfa30126a57b5486dad228cf
Michael Freeman <michaeljrfreeman@gmail.com>
2023-10-15 11:01:38 +0000

    full responsive

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,62 +1,139 @@
-import * as React from "react";;
+import * as React from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
 import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+import AdbIcon from "@mui/icons-material/Adb";
 import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
 import { truncate } from "../utils/string";
 require("@solana/wallet-adapter-react-ui/styles.css");
 
 const pages = ["Home", "Features", "Testimonials", "FAQs"];
+const settings = ["Profile", "Account", "Dashboard", "Logout"];
 
 function NavAppBar({ connected, publicKey }) {
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
   return (
-    <AppBar position="static" sx={{ background: "#2f3030", opacity: "0.9" }}>
+    <AppBar position="static" sx={{ background: "#4E342E" }}>
       <Container maxWidth="xl">
         <Toolbar disableGutters>
+          {/* <AdbIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} /> */}
           <Typography
             variant="h6"
-            wrap="true"
+            noWrap
+            component="a"
+            href="#app-bar-with-responsive-menu"
+            sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex' },
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            Terra Vault
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: "block", md: "none" },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">{page}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          {/* <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} /> */}
+          <Typography
+            variant="h5"
+            noWrap
             component="a"
             href="/"
             sx={{
               mr: 16,
-              display: { xs: "none", md: "flex" },
+              display: { xs: "flex", md: "none" },
+              flexGrow: 1,
               fontFamily: "monospace",
               fontWeight: 700,
+              letterSpacing: ".3rem",
               color: "inherit",
               textDecoration: "none",
             }}
           >
-            TERRA VAULT
+            Terra Vault
           </Typography>
-          <Box
-            sx={{
-              flexGrow: 1,
-              display: "flex",
-              justifyContent: "space-between",
-            }}
-          >
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
             {pages.map((page) => (
-              <Button key={page} sx={{ color: "white", display: "block" }}>
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: "white", display: "block" }}
+              >
                 {page}
               </Button>
             ))}
-            <WalletMultiButton className="wallet-connect">
-              <span>
-                {connected ? truncate(publicKey.toString()) : "Connect Wallet"}
-              </span>
-            </WalletMultiButton>
           </Box>
 
-          {/* <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-              </IconButton>
-            </Tooltip>
+          <Box sx={{ flexGrow: 0 }}>
+            <WalletMultiButton className="wallet-connect text-white">
+              <span className="hidden md:inline">
+                {connected ? truncate(publicKey.toString()) : "Connect Wallet"}
+              </span>
+              <span className="inline md:hidden">Wallet</span>
+            </WalletMultiButton>
             <Menu
               sx={{ mt: "45px" }}
               id="menu-appbar"
@@ -79,7 +156,7 @@ function NavAppBar({ connected, publicKey }) {
                 </MenuItem>
               ))}
             </Menu>
-          </Box> */}
+          </Box>
         </Toolbar>
       </Container>
     </AppBar>
