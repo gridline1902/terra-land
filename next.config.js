47291a96b1bd178311a9326d282349402d0c23ee
Michael Freeman <michaeljrfreeman@gmail.com>
2023-10-15 07:58:26 +0000

    eslint ignored

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    pageExtensions: ['mdx', 'md', 'jsx', 'js', 'tsx', 'ts'],
-}
-
-module.exports = nextConfig
+export const eslint = {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+};
