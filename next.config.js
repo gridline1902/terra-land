b10223aaa426464bd7c5ab0f34d1c754e0e3198f
Michael Freeman <michaeljrfreeman@gmail.com>
2023-10-15 08:03:37 +0000

    set to module.exports

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
-export const eslint = {
+module.exports = {
+  eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
-};
+  },
+}
